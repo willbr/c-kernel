5f038084906262ab766dd21772f9d9f86e6689a9
William Bettridge-Radford <william.bettridge.radford@gail.com>
2022-12-23 21:35:56 +0000

    add double_int and double_float

--- lib-hello.c
+++ lib-hello.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 
-__declspec(dllexport)
+#define DLLExport __declspec(dllexport)
+
+DLLExport
 void
 say_hello(void) {
     puts("hello");
+}
+
+
+DLLExport
+int
+double_int(int n) {
+    return n * 2;
+}
+
+DLLExport
+float
+double_float(float n) {
+    return n * 2;
 }
 
--- main.py
+++ main.py
@@ -3,3 +3,11 @@ from ctypes import *
 dll = WinDLL('./lib-hello.dll')
 dll.say_hello()
 
+n = dll.double_int(9)
+print(n)
+
+dll.double_float.argtypes = [c_float]
+dll.double_float.restype = c_float
+
+f = dll.double_float(3.14)
+print(f"{f:0.2f}")
